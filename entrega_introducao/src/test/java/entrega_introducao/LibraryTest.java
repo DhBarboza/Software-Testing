c4b60f61d68a73dcacf9394ff01e084ea7b216ec
Douglas Henrique <barbozadh99@gmail.com>
2020-09-15 16:53:52 -0300

    Update Refatoring

--- entrega_introducao/src/test/java/entrega_introducao/LibraryTest.java
+++ entrega_introducao/src/test/java/entrega_introducao/LibraryTest.java
@@ -7,24 +7,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
+    //Test Method  
+    //Dada a temperatura 20ºc Celsius o Resultado esperado é 68ºf
     @Test public void testCoverterCtF(){
         Conversor convert = new Conversor();
         double results = convert.celsiusToFahrenheit(20);
         assertEquals(68, results, 0);
     }
 
+    //Dado a temperatura 68ºf fahrenheit o resultado esperado é 20ºc Celsius
     @Test public void testCoverterFtC() {
         Conversor convert = new Conversor();
         double result = convert.fahrenheitToCelsius(68);
         assertEquals(20, result, 0);
     }
-
+    
+    //Dado a temperatura 20ºc Celsius o resultado esperado é 293.15k Kelvin
     @Test public void testCoverterCtK() {
         Conversor convert = new Conversor();
         double result = convert.celsiusToKelvin(20);
         assertEquals(293.15, result, 0);
     }
 
+    //Dado a Temperatura 293.15ºk Kelvin o resultado esperado é 20ºc Celsius
     @Test public void testCoverterKtC() {
         Conversor convert = new Conversor();
         double result = convert.kelvinToCelsius(293.15);
